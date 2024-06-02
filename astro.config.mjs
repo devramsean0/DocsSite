048f4b47857833b92e39dde070690c6d57854124
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2024-06-02 18:37:12 +0100

    Other unstaged changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import solidJs from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel/serverless";
+import markdoc from "@astrojs/markdoc";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solidJs(), markdoc(), tailwind(), sitemap()],
+  output: "server",
+  adapter: vercel()
+});
